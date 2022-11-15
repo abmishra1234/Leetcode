37a1a0751f6ee0aae99a92a75cc38cd2a20b632f
Abinash Kumar Mishra <abmishra1234@gmail.com>
2022-11-15 07:07:41 +0530

    Initial commit

---
+++ Source.cpp
@@ -0,0 +1,36 @@
+using namespace std;
+#include<iostream>
+#include<vector>
+#include<string>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<unordered_set>
+#include<list>
+#include<stack>
+#include<queue>
+#include<algorithm>
+
+//#define FORREF
+#ifndef FORREF
+
+
+int main()
+{
+	cout << "Hello !!" << endl;
+
+
+
+	return 0;
+}
+
+
+
+
+
+
+
+#endif // FORREF
+
+
+
