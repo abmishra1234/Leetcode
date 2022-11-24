bbf3658512ed2d66d3266683f577c18e31ba556d
Abinash Kumar Mishra <abmishra1234@gmail.com>
2022-11-24 22:36:22 +0530

    Small problem in code , lets finish that in morning...

--- DiagonalTraverse.cpp
+++ DiagonalTraverse.cpp
@@ -1,5 +1,4 @@
 using namespace std;
-
 #include<iostream>
 #include<vector>
 #include<string>
@@ -11,76 +10,102 @@ using namespace std;
 #include<stack>
 #include<queue>
 #include<algorithm>
-
-// Diagonal Traversing 
+// Diagonal Traversing  
 //#define FORREF
 #ifndef FORREF
-
 class Solution {
-public:
-    void printMatrix(vector<vector<int>>& mat) {
-        for (auto v : mat) {
-            for (auto elem : v) {
-                cout << elem << " ";
-            }
-            cout << endl;
+    // move method for moving the forward direction
+    void move(bool forward, int& r, int& c) {
+        if (forward) {
+            r--;
+            c++;
         }
-        cout << endl;
-    }
-    // What is the Dynamic Array Elements here
-    void printDynamicArray(vector<int> arr) {
-        for (int elem : arr) {
-            cout << elem << " ";
+        else
+        {
+            r++;
+            c--;
         }
-        cout << endl;
     }
+    void check(int& r, int& c, const int nr, const int nc, bool &forward) {
+        bool change = false;
+        // for row
+        if (r < 0) {
+            r = 0;
+            forward = !forward;
+        }
+        
+        if (r >= nr) {
+            r = nr - 1;
+            c++;
+            forward = !forward;
+            check(r, c, nr, nc, forward);
+        }
 
+        // for column
+        if (c < 0) {
+            c = 0;
+            change = true;
+        }
+        
+        if (c >= nc) {
+            c = nc - 1;
+            r++;
+            forward = !forward;
+            check(r, c, nr, nc, forward);
+        }
+    }
 public:
-    // nr is the number of rows in grid or matrix
-    // nc is the number of cols in grid or matrix
-    bool isValidPosition(int r, int c, int nr, int mc) {
-        // @Note : Considering that r and c is the 0 based index here.
-        if (r < 0 || r >= nr)
-            return false;
+    /*
+        TODO : DO the dry run of the code
 
-        if (c < 0 || c >= mc)
-            return false;
-
-        return true;
-    }
-
+    */
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
-        // to collect your answer in this vector
-        vector<int> out;
+        vector<int> result;
+        int nr = (int) mat.size();
+        cout << "nr = " << nr << endl; // 3
+        
+        if (nr == 0) {
+            cout << "Matrix dimension is wrong, please takecare!!!" << endl;
+            return result;
+        }
 
-        // starting from diagonal up
+        int nc = (int) mat[0].size();
+        cout << "nc = " << nc << endl; // 3
+
+        int elementCnt = nr * nc;
+        cout << "elementCnt = " << elementCnt << endl; // 9
 
 
+        int r = 0;
+        int c = 0;
+        bool forward = true;
 
-
-
-        return out;
+        while (elementCnt > 0) {
+            check(r, c, nr, nc, forward); // {-1, 1}
+            result.push_back(mat[r][c]); // the element pushed : 1, 2
+            --elementCnt;
+            move(forward, r, c); // r = -1, c = 1
+        }
+        return result;
     }
-
-
-
-
 };
 
 Solution sln;
 
-
 int main() {
-    vector<vector<int>> mat = { {1, 2, 3},{4, 5, 6},{7, 8, 9} };
-    // for making your ansewer
-    sln.printMatrix(mat);
-    vector<int> answer;
-    answer = sln.findDiagonalOrder(mat);
-
-
+    cout << "Lets begin..." << endl;
+    vector<vector<int>> mat = { {1,2,3}, {4,5,6}, {7,8,9} };
+    vector<int> result = sln.findDiagonalOrder(mat);
+    
+    // print the result below for verification
+    for (auto elem : result) {
+        cout << elem << " ";
+    }
+    cout << endl;
 
     return 0;
 }
+
 
 
 
