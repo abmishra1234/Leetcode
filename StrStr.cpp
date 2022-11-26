74cecd1052118a2f3193017a650a31c77c1f6b7a
Abinash Kumar Mishra <abmishra1234@gmail.com>
2022-11-26 06:35:09 +0530

    Pattern search using string class method

---
+++ StrStr.cpp
@@ -0,0 +1,31 @@
+using namespace std;
+#include<iostream>
+#include<vector>
+#include<string>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<unordered_set>
+#include<list>
+#include<stack>
+#include<queue>
+#include<algorithm>
+
+#define FORREF
+#ifndef FORREF
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        return haystack.find(needle);
+    }
+};
+
+
+int main(void)
+{
+
+    return 0;
+}
+
+#endif // FORREF
