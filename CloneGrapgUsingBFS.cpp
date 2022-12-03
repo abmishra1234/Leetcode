8c11c9be6d74162fac011b98749ac29b6bf05353
Abinash Kumar Mishra <abmishra1234@gmail.com>
2022-12-03 13:53:51 +0530

    Clone Graph using BFS completed
    Clone Graph using BFS completed

--- CloneGrapgUsingBFS.cpp
+++ CloneGrapgUsingBFS.cpp
@@ -53,15 +53,73 @@ public:
 class Solution {
 public:
     Node* cloneGraph(Node* node) {
+        // if the passed graph is empty, than their clone is also empty
+        // and return from here only
+        Node* clone_node = nullptr;
+        if (!node) return clone_node;
 
+        unordered_map<Node*, Node*> mapped;
+        list<Node*> q; // creating one queue here for managing bfs traversing order
+        unordered_set<Node*> visited;
+
+        // Let's create a map to hold the mapping from origional node and new node
+        clone_node = new Node(node->val);
+        mapped[node] = clone_node;
+        q.push_back(node);
+        visited.insert(node);
+
+        while (q.empty() == false) {
+            Node* tnode = q.front();
+            q.pop_front();
+
+            for (auto n : tnode->neighbors) {
+                if (visited.find(n) == visited.end()) {
+                    visited.insert(n);
+                    mapped[n] = new Node(n->val);
+                    q.push_back(n);
+                }
+                mapped[tnode]->neighbors.push_back(mapped[n]);
+            }
+        }
+
+        return clone_node;
     }
 
     // method for traversing the bfs way
     vector<int> bfs_traverse(Node* node) {
+        vector<int> result;
+
+        // As you know your this problem have clearly
+        // mentioned that you have unique value for all the node
+        // for now just assuming that this bfs have max size queue of 10000 element
+        vector<bool> visited(10002, false); // 100 X 100
+
+        // This is a queue system for handling of bfs
+        list<Node*>* lp = new list<Node*>();
         
+        // Put your starting node into the queue to start
+        lp->push_back(node);
+        visited[node->val] = true;
+        result.push_back(node->val);
 
+        // iterate your queue
+        while (lp->empty() == false) {
+            Node* tnode = lp->front();
+            lp->pop_front();
+            
+            for (auto n : tnode->neighbors) {
+                if (visited[n->val] == false) {
+                    visited[n->val] = true;
+                    result.push_back(n->val);
+                    lp->push_back(n);
+                }
+            }
+        }
+        
+        for (auto elem : result) cout << elem << " ";
+        cout << endl;
 
-
+        return result;
     }
 };
 
@@ -107,17 +165,20 @@ int main() {
     // there are two different ways to traverse the graph using bfs or dfs
     // Let's do the traversing by using bfs here and chek the result using two vector comparison???
 
+    result_orig = sln.bfs_traverse(node_1);
     Node* clone_node = sln.cloneGraph(node_1);
+    result_clone = sln.bfs_traverse(clone_node);
 
-        
+    bool failed = false;
+    for (int i = 0; i < result_orig.size(); ++i) {
+        if (result_clone[i] != result_orig[i]) {
+            cout << "Failed the comparison!!" << endl;
+            failed = true;
+            break;
+        }
+    }
 
-
-
-
-
-
-
-
+    //if (!failed) cout << "Pass the Test case!!!" << endl;
 
     return 0;
 }
