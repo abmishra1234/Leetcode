f011f3a374611ee293df58aba104ea5fd16350d5
Abinash Kumar Mishra <abmishra1234@gmail.com>
2022-12-05 22:23:00 +0530

    KeysAnd Rooms Leetcode Probelm
    Completed

--- KeysAndRooms.cpp
+++ KeysAndRooms.cpp
@@ -11,80 +11,46 @@ using namespace std;
 #include<queue>
 #include<algorithm>
 
-// @@@Note : Incomplete, Just started....
-
-/*
-    Constraints:
-
-    n == rooms.length
-    2 <= n <= 1000
-    0 <= rooms[i].length <= 1000
-    1 <= sum(rooms[i].length) <= 3000
-    0 <= rooms[i][j] < n
-    All the values of rooms[i] are unique.
-*/
-
-//#define FORREF
+#define FORREF
 #ifndef FORREF
-// L U R B
-int xr[4] = { 0, -1, 0, 1 };
-int yc[4] = { -1, 0, 1, 0 };
-
 class Solution {
 public:
+    /*
+        As per problem statement return true only if
+        you can able to reach all the rooms by exploring the sequence
+        
+        In problem it is given that, your 0th room is opened so any time 
+        you can go inside this room and their after you have to move the 
+        room's whose key you will fins inside this room.
+    */
 
-    bool Valid(int x, int y, const int& R, const int& C) {
-        if (x < 0 || x >= R) return false;
-        if (y < 0 || y >= C) return false;
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        int n = (int) rooms.size();
+        //cout << "Number of rooms = " << n << endl;
+        // As per the problem room 0 can be accessible, so you can just start 
+        // from room 0
 
-        return true;
-    }
+        vector<int> visited(n, false);
+        queue<int> q;
+        int room_cnt = 0;
 
-    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        q.push(0);
+        visited[0] = true;
+        ++room_cnt;
 
-        int R = mat.size();
-        int C = mat[0].size();
-
-        vector<vector<bool>> visited(R, vector<bool>(C, false));
-        // { r,{c,val}}
-        queue<pair<int, int>> q;
-
-        /*
-            one guiding factor, I can see here that your problem
-            is to fill 0 at all places which is non zero as of now
-
-            And so it is also indicating that your filling zero's should start at all places
-            where you will find zero.
-        */
-
-        vector<vector<int>> dist(R, vector<int>(C, INT_MAX));
-
-        for (int i = 0; i < R; ++i) {
-            for (int j = 0; j < C; ++j) {
-                if (mat[i][j] == 0) {
-                    q.push({ i,j });
-                    dist[i][j] = 0;
-                }
-            }
-        }
-
-        // until your queue becomes empty
         while (q.empty() == false) {
-            pair<int, int> pi = q.front();
+            int r = q.front();
             q.pop();
 
-            int x, y;
-            for (int i = 0; i < 4; ++i) {
-                x = pi.first + xr[i];
-                y = pi.second + yc[i];
-
-                if (Valid(x, y, R, C) && dist[x][y] == INT_MAX) {
-                    dist[x][y] = min(dist[x][y], 1 + dist[pi.first][pi.second]);
-                    q.push({ x,y });
+            for (auto rn : rooms[r]) {
+                if (visited[rn] == false) {
+                    room_cnt++;
+                    visited[rn] = true;
+                    q.push(rn);
                 }
             }
         }
-        return dist;
+        return (room_cnt == n);
     }
 };
 
@@ -92,16 +58,14 @@ Solution sln;
 
 int main(void)
 {
-    vector<vector<int>> input = { {0, 0, 0}, {0, 1, 0}, {0, 0, 0} };
-    vector<vector<int>> result = sln.updateMatrix(input);
+    vector<vector<int>> rooms;
+    rooms = { {1}, {2}, {3}, {} };
+    cout << sln.canVisitAllRooms(rooms) << endl; // true
 
-    for (auto v : result) {
-        for (auto elem : v) {
-            cout << elem << " ";
-        }
-        cout << endl;
-    }
-    cout << endl;
+    rooms = { {1,3} ,{3,0,1},{2},{0} };
+    cout << sln.canVisitAllRooms(rooms) << endl; // false
+
+
 
     return 0;
 }
