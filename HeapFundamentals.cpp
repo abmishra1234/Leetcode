0888613261459b8a54cab64254e88ec0c119e715
Abinash Kumar Mishra <abmishra1234@gmail.com>
2022-12-06 09:43:15 +0530

    Fundamentals of Priority Queue for implementing Max Heap and Min Heap

--- HeapFundamentals.cpp
+++ HeapFundamentals.cpp
@@ -13,10 +13,60 @@ using namespace std;
 
 //#define FORREF
 #ifndef FORREF
+class Solution {
+    priority_queue<int, vector<int>, less<int>> *pMaxHeap;
+    priority_queue<int, vector<int>, greater<int>> *pMinHeap;
+public:
+    Solution() {
+        pMaxHeap = new priority_queue<int, vector<int>, less<int>>();
+        pMinHeap = new priority_queue<int, vector<int>, greater<int>>();
+    
+    }
+    
+    // Let's write one Add method for your max nad min heap
+    void Add(int elem) {
+        pMaxHeap->push(elem);
+        pMinHeap->push(elem);
+    }
 
+    void Top(int &maxval, int &minval) {
+        maxval = pMaxHeap->top();
+        minval = pMinHeap->top();
+
+    }
+
+    // Delete
+    void Delete() {
+        pMaxHeap->pop();
+        pMinHeap->pop();
+    }
+
+    
+
+};
+
+Solution sln;
 
 int main(void)
 {
+    sln.Add(5);
+    sln.Add(2);
+    sln.Add(8);
+    sln.Add(7);
+    sln.Add(6);
+    sln.Add(15);
+    sln.Add(12);
+
+    int max = 0;
+    int min = 0; 
+
+    sln.Top(max, min);
+    cout << "max = " << max << ", min = " << min << endl;
+
+    sln.Delete();
+    sln.Top(max, min);
+    cout << "max = " << max << ", min = " << min << endl;
+
 
     return 0;
 }
