80cdf3e648960ae415c2f26fee083ab203b9750a
Abinash Kumar Mishra <abmishra1234@gmail.com>
2022-12-07 08:56:06 +0530

    temp saving

---
+++ TheKWeakestRowsInaMatrix.cpp
@@ -0,0 +1,31 @@
+using namespace std;
+#include<iostream>
+#include<vector>
+#include<string>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<unordered_set>
+#include<list>
+#include<stack>
+#include<queue>
+#include<algorithm>
+
+//#define FORREF
+#ifndef FORREF
+
+class Solution {
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+
+    }
+};
+
+int main(void)
+{
+
+
+    return 0;
+}
+
+#endif // FORREF
