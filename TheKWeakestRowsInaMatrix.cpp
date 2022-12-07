acd5f947d007a4e0b6a1aea7c0300f606a548a7a
Abinash Kumar Mishra <abmishra1234@gmail.com>
2022-12-07 21:59:03 +0530

    The K Weakest Rows in a Matrix
    The K Weakest Rows in a Matrix

--- TheKWeakestRowsInaMatrix.cpp
+++ TheKWeakestRowsInaMatrix.cpp
@@ -14,10 +14,43 @@ using namespace std;
 //#define FORREF
 #ifndef FORREF
 
+typedef pair<int, int> pii;
+struct MyCmp {
+    auto operator()(const pii& a, const pii& b) const {
+        if (a.first == b.first) return a.second < b.second;
+        return a.first < b.first;
+    }
+};
+
+typedef set<pii, MyCmp> RowSet;
+RowSet rs;
+
+
 class Solution {
 public:
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        rs.clear();
 
+        int ri = 0;
+        for (auto v : mat) {
+            int one_cnt = 0;
+            for (auto elem : v) {
+                if (elem) ++one_cnt;
+            }
+            rs.insert({ one_cnt, ri });
+            ++ri;
+        }
+        
+        vector<int> result;
+
+        while (k > 0 && !rs.empty()) {
+            auto it = rs.begin();
+            result.push_back(it->second);
+            rs.erase(it);
+            --k;
+        }
+
+        return result;
     }
 };
 
