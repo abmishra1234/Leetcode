af37fd7aa77b1722bd36d372d45bebbeb2c81e25
Abinash Kumar Mishra <abmishra1234@gmail.com>
2022-12-10 09:01:38 +0530

    Create 50_Medium_Leetcode.cpp
    Completed

---
+++ 50_Medium_Leetcode.cpp
@@ -0,0 +1,47 @@
+using namespace std;
+#include<iostream>
+#include<vector>
+#include<string>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<unordered_set>
+#include<list>
+#include<stack>
+#include<queue>
+#include<algorithm>
+
+#define FORREF
+#ifndef FORREF
+
+class Solution {
+public:
+    double fastPow(double x, long long n) {
+        if (n == 0) {
+            return 1.0;
+        }
+        double half = fastPow(x, n / 2);
+        if (n % 2 == 0) {
+            return half * half;
+        }
+        else {
+            return half * half * x;
+        }
+    }
+    double myPow(double x, int n) {
+        long long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        return fastPow(x, N);
+    }
+};
+
+int main(void)
+{
+
+    return 0;
+}
+
+#endif // FORREF
