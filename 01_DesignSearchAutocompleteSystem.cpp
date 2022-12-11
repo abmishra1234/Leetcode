533a492d46221834217f5ca865f28ca947d27296
Abinash Kumar Mishra <abmishra1234@gmail.com>
2022-12-11 19:06:49 +0530

    Not completed, taking break from this question , will come back after some time
    Not completed, taking break from this question , will come back after some time

--- 01_DesignSearchAutocompleteSystem.cpp
+++ 01_DesignSearchAutocompleteSystem.cpp
@@ -12,145 +12,29 @@ using namespace std;
 #include<algorithm>
 
 #define _CRT_SECURE_NO_WARNINGS
-
+/*
+    This is not completed yet, Please complete this in sometime...
+*/
 // Approach 01
-//#define FORREF
+#define FORREF
 #ifndef FORREF
-typedef pair<string, int> psi;
-vector<psi> worddb;
-int db_ind;
-
-struct MyCmp {
-    bool operator()(const int i, const int j) const {
-        if (worddb[i].second == worddb[j].second) {
-            // using std string class < oprator
-            // @Note , I hope this won't have any issue...
-            return worddb[i].first.compare(worddb[j].first) < 0;
-        }
-        return worddb[i].second > worddb[j].second;
-    }
-};
-
-typedef set<int, MyCmp> MySet;
-unordered_map<string, int> m;
-
-int getcode(char ch) {
-    if (ch == ' ') return 26;
-    return ch - 'a';
-}
-
-struct TrieNode {
-    bool wc;
-    TrieNode* ch[27];
-    // this is to be considered as Set on tip of prefix
-    // It will hold the recommendation word at the tip of prefix
-    MySet* pSet;  // 04 byte
-    void create() {
-        wc = false;
-        pSet = new MySet();
-        for (int i = 0; i < 27; ++i) ch[i] = nullptr;
-    }
-}pool[100000];
-int pind = 0;
-
-//----------------------------------------
 
 class AutocompleteSystem {
-    TrieNode* root;
-
-    void insert(string word, int pool_ind) {
-        TrieNode* start = root;
-        int i = 0;
-        while (word[i] != '\0') {
-            int code = getcode(word[i]);
-            if (start->ch[code] == nullptr) {
-                start->ch[code] = &pool[pind];
-                start->ch[code]->create();
-                ++pind;
-            }
-            i++;
-            start = start->ch[code];
-            start->pSet->insert(pool_ind);
-            if (word[i] == '\0') {
-                start->wc = true;
-                break;
-            }
-        }
-    }
-
-    vector<string> search(string word) {
-        vector<string> out;
-        TrieNode* start = root;
-        int i = 0;
-        while (word[i] != '\0') {
-            int code = getcode(word[i]);
-            if (start->ch[code] == nullptr) {
-                return {};
-            }
-            start = start->ch[code];
-            i++;
-            if (word[i] == '\0') {
-                // populate your out here
-                int K = 3;
-                auto it = start->pSet->begin();
-                while (K > 0 && it != start->pSet->end()) {
-                    out.push_back(worddb[*it].first);
-                    K--, it++;
-                }
-                break;
-            }
-        }
-        return out;
-    }
-    
-    string prefixword;
-
 public:
     AutocompleteSystem(vector<string>& sentences, vector<int>& times) {
-        // for root to initialised
-        pind = 0;
-        root = &pool[pind++];
-        root->create();
 
-        m.clear();
-        worddb.clear();
-        worddb.resize(100000);
-        db_ind = 0;
-        for (auto i = 0; i < sentences.size(); ++i) {
-            worddb[i] = { sentences[i], times[i] };
-            m[sentences[i]] = i;
-            insert(sentences[i], i);
-            db_ind++;
-        }
-        prefixword = "";
     }
 
     vector<string> input(char c) {
-        vector<string> result;
-        if (c == '#') {
-            /*
-                How you will handle the repetation of sentence in the dictionary.
-                for that purpose we should create the one map of string vs cnt to maintain the
-            */
-
-            auto it = m.find(prefixword);
-            if (it == m.end()) {
-                worddb[db_ind] = { prefixword, 1 };
-                m[prefixword] = db_ind;
-            }
-            else worddb[it->second].second++;
-            insert(prefixword, db_ind);
-            prefixword = "";
-            result = {};
-        }
-        else {
-            prefixword += c;
-            result = search(prefixword);
-        }
-        return result;
+        return {};
     }
 };
 
+/*
+    Your TC are not strong enough, please build some of your own TC
+    to verify your logic and complete it.
+    This is your learning, to get the right motivation over the quick coding 
+*/
 int main(void)
 {
     // write some verification code first
