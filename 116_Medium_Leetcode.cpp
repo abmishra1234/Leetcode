2037bc96da7f92ec39c566c7113d0c7e278f04e7
Abinash Kumar Mishra <abmishra1234@gmail.com>
2022-12-12 22:57:45 +0530

    Saving intermediate code

--- 116_Medium_Leetcode.cpp
+++ 116_Medium_Leetcode.cpp
@@ -29,7 +29,7 @@ public:
         : val(_val), left(_left), right(_right), next(_next) {}
 };
 
-#define MAXN 4096
+#define MAXN 4100
 
 typedef pair<Node*, int> ppi; // <Node* , level>
 queue<ppi> q;
@@ -45,6 +45,48 @@ public:
     Node* connect(Node* root) {
         
         return nullptr;
+    }
+
+    /*
+        Let's modify your level order code for maintaining the 
+        level by level of the tree node
+    */
+    void levelorder(Node* root, vector<vector<Node*>> &leveldata) {
+        if (!root) return;
+
+        // the first thing you should do to clean the queue
+        while (q.empty() == false) q.pop();
+
+        int level = 1;
+        // push the starting node into the queue
+        q.push({ root, level });
+        leveldata[level].push_back(root);
+
+        cout << endl;
+        while (q.empty() == false) {
+            ppi p = q.front();
+            q.pop();
+
+            cout << "val = " << p.first->val
+                << ", level = " << p.second << endl;
+
+            if (p.first->left) {
+                q.push({ p.first->left, p.second + 1 });
+                leveldata[p.second + 1].push_back(p.first->left);
+            }
+
+            if (p.first->right) {
+                q.push({ p.first->right, p.second + 1 });
+                leveldata[p.second + 1].push_back(p.first->right);
+            }
+        }
+        
+        // now you have level data, it's time to set next pointer
+        // properly
+
+    
+    
+    
     }
 
     // Let's write logic for traversing the 
@@ -98,7 +140,9 @@ public:
     void buildtree(const vector<int> &v) {
         root = buildtree_util(v, 0, root);
         inorder(root);
-        levelorder(root);
+        // Just clear the vector if anything already exist
+        vector<vector<Node*>> leveldata(MAXN, vector<Node*>());
+        levelorder(root, leveldata);
     }
 };
 
