9e67da1827109259ca4d46db6f5bd8a9771a06d2
Abinash Kumar Mishra <abmishra1234@gmail.com>
2022-12-16 18:24:21 +0530

    Completed

--- 489_Hard_Leetcode.cpp
+++ 489_Hard_Leetcode.cpp
@@ -17,85 +17,145 @@ using namespace std;
 //#define FORREF
 #ifndef FORREF
 
-// U->R->D->L
-int x_dir[4] = { -1, 0, 1, 0 }; 
-int y_dir[4] = { 0, 1, 0, -1 };
+// the below chunk is for movement
+int dx[4] = { -1, 0, 1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
 
-// for maintaining the direction
-// U->R->D->L
-int dir[4] = { 0, 1, 2, 3 };
 
-/*
-    Complete the Driver code later.
-*/
 class Robot {
+public:
+    int maxR, maxC;// Number of rows and cols in grid
+    vector<vector<int>> room; // this si the room divided into grid form of cells
+    vector<vector<bool>> visited; // this si the room divided into grid form of cells
 
+    int currRow, currCol; // this is for starting the robot move 
+    int dir; // this is for moving direction
+
+    // the below chunk is for movement
+    int dx[4] = { -1, 0, 1, 0 };
+    int dy[4] = { 0, 1, 0, -1 };
+
+    // To maintain the freecell count covered or not?
+    int freecellcnt;
+
+    /*
+        Completed but not verified the logic with dry run
+    */
+    bool valid(int x, int y) {
+        if (x < 0 || x >= maxR) return false;
+        if (y < 0 || y >= maxC) return false;
+
+        return true;
+    }
+
+    /*
+        Completed but not verified the logic with dry run
+    */
+    bool move() {
+        int x = currRow + dx[dir];
+        int y = currCol + dy[dir];
+
+        if (!valid(x, y)) return false;
+        if (!room[x][y]) return false;
+        if (visited[x][y]) return false;
+        
+        currRow = x;
+        currCol = y;
+               
+        return true;
+    }
+    /*
+        Completed but not verified the logic with dry run
+    */
+    void clean() {
+        // If already clean than don't don any thing just return
+        if (!visited[currRow][currCol]) {
+            --freecellcnt;
+            visited[currRow][currCol] = 1;
+        }
+        else
+            cout << "Cell is already cleaned so return without doing any thing..." << endl;
+    }
+    /*
+        Completed but not verified the logic with dry run
+    */
+    void turnRight() {
+        // turn right is clockwise move ment
+        dir = (dir + 1) % 4;
+    }
+    /*
+        Completed but not verified the logic with dry run
+    */
+    void turnLeft() {
+        dir = ((dir - 1) < 0) ? ((dir - 1) + 4) : dir - 1;
+    }
 };
 
 typedef pair<int, int> pii;
-#define MP make_pair
+struct MyHash {
+    auto operator()(const pii& p) const {
+        return std::hash<int>{}(p.first) ^ std::hash<int>{}(p.second);
+    }
+};
+typedef unordered_set<pii, MyHash> MySet;
+
 class Solution {
 public:
-    int dx[4] = { 0, -1, 0, 1 };
-    int dy[4] = { 1, 0, -1, 0 };
+    Robot* rp;
+    MySet visited;
 
-    void moveForward(Robot& robot, int x, int y, int direction, map<pii, bool>& visited) {
-        // Try to move forward if possible
-        if (!visited[MP(x + dx[direction], y + dy[direction])] && robot.move()) {
-            visited[MP(x + dx[direction], y + dy[direction])] = 1;
+    // the below chunk is for movement
+    int dx[4] = { -1, 0, 1, 0 };
+    int dy[4] = { 0, 1, 0, -1 };
 
-            // Now do further depth first search
-            DFS(robot, x + dx[direction], y + dy[direction], direction, visited);
-
-            // Come back to your position
-            robot.turnRight();
-            robot.turnRight();
-            robot.move();
-            robot.turnRight();
-            robot.turnRight();
-        }
-    }
-
-    void DFS(Robot& robot, int x, int y, int direction, map<pii, bool>& visited) {
-        // First clean
+    void dfs(int x, int y, int dir, MySet& visited, Robot &robot) {
+        visited.insert({ x,y });
         robot.clean();
 
-        // Then try moving foward in current direction and all other directions
-        // N
-        moveForward(robot, x, y, direction, visited);
+        // check all the 04 direction
+        for (int i = 0; i < 4; ++i) {
+            int newdir = (dir + i) % 4;
+            int newx = x + dx[newdir];
+            int newy = y + dy[newdir];
 
-        // W
-        robot.turnRight();
-        direction = (direction + 1) % 4;
-        moveForward(robot, x, y, direction, visited);
-
-        // S
-        robot.turnRight();
-        direction = (direction + 1) % 4;
-        moveForward(robot, x, y, direction, visited);
-
-        // E
-        robot.turnRight();
-        direction = (direction + 1) % 4;
-        moveForward(robot, x, y, direction, visited);
-
-        // N
-        robot.turnRight();
-        direction = (direction + 1) % 4;
+            if (visited.find({ newx, newy }) == visited.end() && robot.move() ) {
+                dfs(newx, newy, dir, visited, robot);
+            }
+            robot.turnRight();
+        }
+        // now the next set of call is for backtrack
+        robot.turnLeft();
+        robot.turnLeft();
+        robot.move();
+        robot.turnLeft();
+        robot.turnLeft();
     }
 
     void cleanRoom(Robot& robot) {
-        map<pii, bool> visited;
+        rp = &robot;
+        visited.clear();
 
-        DFS(robot, 0, 0, 0, visited);
+        // Since you are not sure the position from where you will start, so the only
+        // Solution to this is backtrack approach
+        dfs(0, 0, 0, visited, robot);
     }
 };
 
 
-Solution sln;
 
 int main(void)
 {
+    // Let write some verification test cases below
+    vector<vector<int>> grid 
+        = { {1,1,1,1,1,0,1,1},{1,1,1,1,1,0,1,1}, 
+            {1,0,1,1,1,1,1,1}, {0,0,0,1,0,0,0,0}, 
+            {1,1,1,1,1,1,1,1 } };
+    int _sr = 1, _sc = 3, _dir = 0;
+
+    Robot* rp = new Robot();
+    Solution sln;
+    sln.cleanRoom(*rp);
+    delete rp;
 
     return 0;
 }
