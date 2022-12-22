438dd087887166b66770486aa9cb794fa25ec3b5
Abinash Kumar Mishra <abmishra1234@gmail.com>
2022-12-23 05:17:33 +0530

    Update UnionFind_DS_Implementation.cpp

--- UnionFind_DS_Implementation.cpp
+++ UnionFind_DS_Implementation.cpp
@@ -11,12 +11,55 @@ using namespace std;
 #include<queue>
 #include<algorithm>
 
-//#define FORREF
+#define FORREF
 #ifndef FORREF
 
 /*
     Union Find  or Disjoint Set Implementation
+    Not yest completed , and complete this problem with Disjoint Set approach later...
 */
+class UnionFind {
+    typedef unordered_map<int, int> UMAP;
+    UMAP parent;
+    UMAP rank;
+
+    int nvertices;
+    
+    void precompute(int n)
+    {
+        nvertices = n;
+        for (int v = 1; v <= n; ++v) {
+            parent[v] = v;
+            rank[v] = 0;
+        }
+    }
+
+    // returning the root of vertex set
+    int Find(int v) {
+        if (parent[v] != v) return Find(parent[v]);
+        return v;
+    }
+
+    bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
+        precompute(n);
+
+        for (int i = 0; i < dislikes.size(); ++i) {
+            int u = dislikes[i][0];
+            int v = dislikes[i][1];
+
+            int x = Find(u);
+            int y = Find(v);
+
+            if (x == y) return false;
+
+            parent[u] = x;
+            parent[v] = y;
+        }
+        return true;
+    }
+};
+
+
 
 
 int main(void)
