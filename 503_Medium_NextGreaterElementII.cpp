1a52230ec1d2a11df56da3684755449799d7adda
Abinash Kumar Mishra <abmishra1234@gmail.com>
2022-12-25 14:33:44 +0530

    Completed

--- 503_Medium_NextGreaterElementII.cpp
+++ 503_Medium_NextGreaterElementII.cpp
@@ -18,11 +18,26 @@ using namespace std;
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
+        stack<int> s;
+        vector<int> result;
+        int sz = nums.size();
 
+        for (int i = sz - 2; i >= 0; --i) {
+            // you are moving from right to left
+            s.push(nums[i]);
+        }
 
+        for (int i = sz - 1; i >= 0; --i) {
+            // you are moving from right to left
+            while (!s.empty() && s.top() <= nums[i]) s.pop();
+            if (s.empty()) result[i] = -1;
+            else {
+                result[i] = s.top();
+                s.push(nums[i]);
+            }
+        }
 
-
-        return {};
+        return result;
     }
 };
 
