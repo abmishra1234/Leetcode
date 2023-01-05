543bd167afd059f77d7a7c9a1b69ce28bfdd73d1
Abinash Kumar Mishra <abmishra1234@gmail.com>
2023-01-05 09:11:28 +0530

    740 Leetcode Problem completed
    740 Leetcode Problem completed

--- 740_Medium_DeleteAndEarn.cpp
+++ 740_Medium_DeleteAndEarn.cpp
@@ -18,48 +18,29 @@ using namespace std;
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
-        // first sort this 
-        sort(nums.begin(), nums.end());
         // Merge the element together of same value
-
         int val = nums[0];
         int sum = 0;
-
-        vector<int> merged;
+        vector<int> merged(10005, 0);
 
         for (int i = 0; i < nums.size(); ++i) {
-            if (val == nums[i]) {
-                sum += nums[i];
-            }
-            else
-            {
-                merged.push_back(sum);
-                val = nums[i];
-                sum = val;
-            }
-
-            if (i == nums.size() - 1) {
-                merged.push_back(sum);
-            }
+            merged[nums[i]] += nums[i];
         }
 
         int maxv = 0;
-
         int ns = merged.size();
-        if (ns == 1) return merged[0];
-        else if (ns == 2) return max(merged[0], merged[1]);
-        else {
-            vector<int> dp;
-            dp.resize(ns, 0);
 
-            dp[0] = merged[0];
-            dp[1] = max(merged[0], merged[1]);
+        vector<int> dp;
+        dp.resize(10005, 0);
 
-            for (int i = 2; i < ns; ++i) {
-                dp[i] = max(merged[i] + dp[i - 2], dp[i - 1]);
-                maxv = max(maxv, dp[i]);
-            }
+        dp[0] = merged[0];
+        dp[1] = max(merged[0], merged[1]);
+
+        for (int i = 2; i < ns; ++i) {
+            dp[i] = max(merged[i] + dp[i - 2], dp[i - 1]);
+            maxv = max(maxv, dp[i]);
         }
+
         return maxv;
     }
 };
