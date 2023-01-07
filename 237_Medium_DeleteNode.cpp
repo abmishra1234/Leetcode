b40c2205fc9e4cb6c93208dc48db680bf46e53f3
Abinash Kumar Mishra <abmishra1234@gmail.com>
2023-01-07 19:59:20 +0530

    Completed 237

---
+++ 237_Medium_DeleteNode.cpp
@@ -0,0 +1,42 @@
+using namespace std;
+#include<iostream>
+#include<vector>
+#include<string>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<unordered_set>
+#include<list>
+#include<forward_list>
+#include<stack>
+#include<queue>
+#include<algorithm>
+#include<cstring>
+
+#define FORREF
+#ifndef FORREF
+class ListNode {
+public:
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* n) : val(x), next(n) {}
+};
+
+class Solution {
+public:
+    void deleteNode(ListNode* &node) {
+        // copy the next element
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
+
+int main(void)
+{
+
+    return 0;
+}
+
+#endif // FORREF
