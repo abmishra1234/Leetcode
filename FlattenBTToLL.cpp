19344149a93c54df082778442834067f22ab99df
Abinash Kumar Mishra <abmishra1234@gmail.com>
2023-01-10 10:58:12 +0530

    temp save

---
+++ FlattenBTToLL.cpp
@@ -0,0 +1,42 @@
+// 114. Flatten Binary Tree to Linked List
+using namespace std;
+#include<iostream>
+#include<vector>
+#include<string>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<unordered_set>
+#include<list>
+#include<forward_list>
+#include<stack>
+#include<queue>
+#include<algorithm>
+#include<cstring>
+
+//#define FORREF
+#ifndef FORREF
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* l, TreeNode* r) : val(0), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+
+    }
+};
+
+int main(void)
+{
+
+    return 0;
+}
+
+#endif // FORREF
