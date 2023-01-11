8b7a3975a0328d1b3c4d9ab1b3dc0ed6d504a343
Abinash Kumar Mishra <abmishra1234@gmail.com>
2023-01-11 18:52:00 +0530

    Completed this problem
    Revision is required

---
+++ 109_Medium_ConvertSortedLL2BST.cpp
@@ -0,0 +1,67 @@
+using namespace std;
+#include<iostream>
+#include<vector>
+#include<string>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<unordered_set>
+#include<list>
+#include<forward_list>
+#include<stack>
+#include<queue>
+#include<algorithm>
+#include<cstring>
+
+//#define FORREF
+#ifndef FORREF
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* n) : val(x), next(n) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left, * right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* l, TreeNode* r) : val(0), left(l), right(r) {}
+};
+
+class Solution {
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        if (!head) return nullptr;
+        if (!head->next) return new TreeNode(head->val);
+
+        // you need to find now the mid point
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        ListNode* mid = slow->next;
+        slow->next = nullptr;
+
+        TreeNode* root = new TreeNode(mid->val);
+        root->left = sortedListToBST(head);
+        root->right = sortedListToBST(mid->next);
+
+        return root;
+    }
+};
+
+int main(void)
+{
+
+    return 0;
+}
+
+#endif // FORREF
