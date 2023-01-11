82315416002012e7c4687830889b89e8568dadd0
Abinash Kumar Mishra <abmishra1234@gmail.com>
2023-01-11 08:03:32 +0530

    Saving

---
+++ 1206_Hard_DesignSkiplist.cpp
@@ -0,0 +1,54 @@
+using namespace std;
+#include<iostream>
+#include<vector>
+#include<string>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<unordered_set>
+#include<list>
+#include<forward_list>
+#include<stack>
+#include<queue>
+#include<algorithm>
+#include<cstring>
+
+//#define FORREF
+#ifndef FORREF
+
+class Skiplist {
+public:
+    Skiplist() {
+
+    }
+
+    bool search(int target) {
+
+    }
+
+    void add(int num) {
+
+    }
+
+    bool erase(int num) {
+
+    }
+};
+
+/**
+ * Your Skiplist object will be instantiated and called as such:
+ * Skiplist* obj = new Skiplist();
+ * bool param_1 = obj->search(target);
+ * obj->add(num);
+ * bool param_3 = obj->erase(num);
+ */
+
+int main(void)
+{
+
+
+
+    return 0;
+}
+
+#endif // FORREF
