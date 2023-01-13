c1324734a07e183c9625ba36020e36f0fb44c431
Abinash Kumar Mishra <abmishra1234@gmail.com>
2023-01-13 07:00:10 +0530

    Completed 2058 Problem
    Completed 2058 Problem

--- 2058_Medium_MinimumAndMaximumNodes.cpp
+++ 2058_Medium_MinimumAndMaximumNodes.cpp
@@ -13,9 +13,77 @@ using namespace std;
 #include<algorithm>
 #include<cstring>
 
-//#define FORREF
+#define FORREF
 #ifndef FORREF
 
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* n) : val(x), next(n) {}
+};
+
+class Solution {
+public:
+    typedef pair<int, int> pii; // < value, position >
+    // For your custom sorting
+    struct MySort {
+        auto operator()(const pii& a, const pii& b) const {
+            return a.second < b.second;
+        }
+    };
+
+    void util(ListNode* head, int pos, stack<pii>& prev,
+        stack<pii>& next, vector<pii>& cp /* critical points*/)
+    {
+        if (!head) return;
+        prev.push({ head->val, pos });
+        util(head->next, pos + 1, prev, next, cp);
+        
+        // stack unwinding step
+        if (prev.size() > 0) {
+            pii curr = prev.top();
+            prev.pop();
+
+            if (prev.size() > 0 && next.size() > 0) {
+                pii t1 = prev.top();
+                pii t2 = next.top();
+
+                if (curr.first > t1.first && curr.first > t2.first
+                    || (curr.first < t1.first && curr.first < t2.first)) {
+                    cp.push_back(curr);
+                }
+            }
+            next.push(curr);
+        }
+    }
+
+    vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+        stack<pii> prev;
+        stack<pii> next;
+        vector<pii> cp;
+
+        util(head, 1, prev, next, cp);
+        if (cp.size() == 0 || cp.size() == 1) return { -1, -1 };
+
+        // now find the min and max distance
+        sort(cp.begin(), cp.end(), MySort());
+        
+        int mind = INT_MAX, maxd = INT_MIN;
+        
+        int vs = (int) cp.size();
+        maxd = max(cp[vs - 1].second - cp[0].second, maxd);
+
+        for (int i = 1; i < vs; ++i) {
+            mind = min(mind, cp[i].second - cp[i - 1].second);
+        }
+
+        return {mind, maxd};
+    }
+};
+
+Solution sln;
 
 int main(void)
 {
