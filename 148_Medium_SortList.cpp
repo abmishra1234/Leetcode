013528472a48fb1a17869a01bfc0e26e24f8e92a
Abinash Kumar Mishra <abmishra1234@gmail.com>
2023-01-13 07:58:46 +0530

    save

---
+++ 148_Medium_SortList.cpp
@@ -0,0 +1,50 @@
+using namespace std;
+#include<iostream>
+#include<vector>
+#include<string>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<unordered_set>
+#include<list>
+#include<forward_list>
+#include<stack>
+#include<queue>
+#include<algorithm>
+#include<cstring>
+
+//#define FORREF
+#ifndef FORREF
+
+/*
+    Approach 01 : One solution which is recursive??
+
+*/
+
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* n) : val(x), next(n) {}
+};
+
+class Solution {
+public:
+
+
+
+
+    ListNode* sortList(ListNode* head) {
+
+    }
+};
+
+int main(void)
+{
+
+    return 0;
+}
+
+#endif // FORREF
