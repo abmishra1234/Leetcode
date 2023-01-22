870fc9b8ce89464da05619e51382d9f87b1e0a23
Abinash Kumar Mishra <abmishra1234@gmail.com>
2023-01-22 19:23:49 +0530

    Update ShortestPathInDAGWeighted.cpp

--- ShortestPathInDAGWeighted.cpp
+++ ShortestPathInDAGWeighted.cpp
@@ -2,3 +2,59 @@
 	https://www.codingninjas.com/codestudio/library/shortest-path-in-a-directed-acyclic-graph
 */
 
+using namespace std;
+#include<iostream>
+#include<vector>
+#include<string>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<unordered_set>
+#include<list>
+#include<forward_list>
+#include<stack>
+#include<queue>
+#include<algorithm>
+#include<cstring>
+
+//#define FORREF
+#ifndef FORREF
+/*
+	Pre-Condition here is
+	1. Directed and Acyclic Graph
+	2. you will be looking for shortest path from one src to n-1 destibnation vertex
+
+	If the above pre - condition met in your problem than you are good to go for the 
+	below type of solution approach
+
+	Approach:
+	As you know, in graph, there are two different traversing algorithm available
+
+	BFS : using queue data structure
+	DFS : using system stack or custom stack
+
+	Usually, when you have large recursion depth possible, in that scenario
+	might be BFS is better solution instead DFS but in general theoritically 
+	BFS and DFS both will work in all the scenario with miner customisation
+	in all graph problem.
+*/
+
+class ShortestPath {
+
+
+
+};
+
+
+
+
+
+int main(void)
+{
+
+	return 0;
+}
+
+#endif // FORREF
+
+
