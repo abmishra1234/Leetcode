325605cf6fb2970f307d9bf4eff419ee6b07124b
Abinash Kumar Mishra <abmishra1234@gmail.com>
2023-01-28 18:11:11 +0530

    Completed

---
+++ 102_Medium_BinaryTreeLevelOrder.cpp
@@ -0,0 +1,64 @@
+// https://leetcode.com/problems/binary-tree-level-order-traversal/description/
+using namespace std;
+#include<iostream>
+#include<vector>
+#include<string>
+#include<unordered_map>
+#include<map>
+#include<unordered_set>
+#include<set>
+#include<list>
+#include<forward_list>
+#include<stack>
+#include<queue>
+#include<algorithm>
+#include<cstring>
+#include<climits>
+
+#define FORREF
+#ifndef FORREF
+
+struct TreeNode {
+	int val;
+	TreeNode* left, * right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* l, TreeNode* r) : val(x), left(l), right(r) {}
+};
+
+class Solution {
+public:
+	vector<vector<int>> levelOrder(TreeNode* root) {
+		if (!root) return {};
+		deque<TreeNode*> q;
+		q.push_back(root);
+
+		// for collecting the level order data
+		vector<vector<int>> ldata; // level by level data
+
+		while (!q.empty()) {
+			int qs = q.size();
+			vector<int> v;
+			for (int i = 0; i < qs; ++i) {
+				TreeNode* np = q.front();
+				q.pop_front();
+
+				if (np->left) q.push_back(np->left);
+				if (np->right) q.push_back(np->right);
+
+				v.push_back(np->val);
+			}
+			ldata.push_back(v);
+		}
+		return ldata;
+	}
+};
+
+
+int main(void)
+{
+
+	return 0;
+}
+
+#endif // FORREF
