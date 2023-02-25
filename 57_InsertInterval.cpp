dca73b4beb124e296537ec22e7771aae03d03d69
Abinash Kumar Mishra <abmishra1234@gmail.com>
2023-02-26 04:48:31 +0530

    Completed 57
    Completed 57

--- 57_InsertInterval.cpp
+++ 57_InsertInterval.cpp
@@ -13,18 +13,64 @@ using namespace std;
 #include<algorithm>
 #include<cstring>
 
-//#define FORREF
+#define FORREF
 #ifndef FORREF
 
 class Solution {
 public:
+    // check the overlapping
+    bool isOverlap(vector<int> &in1, vector<int> &in2) {
+        int diff = min(in1[1], in2[1]) - max(in1[0], in2[0]);
+        return (diff >= 0) ? true : false;
+    }
+    vector<int> merge_interval(const vector<int> &in1, const vector<int> &in2) {
+        return { min(in1[0], in2[0]), max(in1[1], in2[1]) };
+    }
+
+    // For now i am going through the linear search
+    // TBD  - later change this with binary search 
+    void insert_interval(vector<vector<int>>& org_int, vector<int> new_interval) {
+        int n = (int)org_int.size();
+        int i = 0;
+        for (; i < n; ++i) {
+            if (new_interval[0] < org_int[i][0]) break;
+        }
+        org_int.insert(org_int.begin() + i, new_interval);
+    }
+
     vector<vector<int>> insert(vector<vector<int>>& intervals, 
         vector<int>& newInterval) {
-        // TBD
+        // the first thing is to insert the new interval in the given
+        // list of intervals
+        insert_interval(intervals, newInterval);
 
+        vector<vector<int>> output;
+        if (intervals.empty()) {
+            output.push_back(newInterval);
+            return output;
+        }
 
+        int start = intervals[0][0];
+        int end = intervals[0][1];
 
-        return {};
+        for (int i = 1; i < intervals.size(); ++i) {
+            vector<int> current;
+            current.push_back(start);
+            current.push_back(end);
+            if (isOverlap(current, intervals[i]) == false) {
+                // no overlap case
+                output.push_back(current);
+                start = intervals[i][0];
+                end = intervals[i][1];
+            }
+            else {
+                end = max(end, intervals[i][1]);
+            }
+        }
+
+        output.push_back({start, end});
+
+        return output;
     }
 };
 
